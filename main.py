732adfd30e866ac7f3f126d3373eba82040234a4
cryocaustik <cryocaustik@gmail.com>
2017-04-15 19:54:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-from os.path import isfile
-import requests 
+import json
 import mmap 
 from tqdm import tqdm
 
-path = 'dev_pad/py_temp_existing_ids.txt'
+path_ids = './py_temp_existing_ids.txt'
+path_data = './new_ids.json'
+new_results = set()
 
-with open(path, 'r+b') as _file:
+with open(path_data, 'r') as _in: 
+	new_ids = json.load(_in)
+	_in.close()
+
+with open(path_ids, 'r+b') as _file:
     mm = mmap.mmap(_file.fileno(), 0)
-    
-    response = requests.get('https://zkillboard.com/api/history/20161201/')
-    if response.status_code == 200:
-        results = response.json()
-        new_results = set()
-        
-        with tqdm(total=len(results.keys())) as pbar:
-            for _id in results:
-                if mm.find(str(_id).encode()) != -1:
-                    new_results.add((_id, results[_id]))
-                else:
-                    continue
-                pbar.update()
+	
+
+    with tqdm(total=len(new_ids.keys())) as pbar:
+        for _id in new_ids:
+            if not mm.find(str(_id).encode()) != -1:
+                new_results.add((_id, new_ids[_id]))
+            else:
+                continue
+            pbar.update()
     mm.close()
     _file.close()
     
